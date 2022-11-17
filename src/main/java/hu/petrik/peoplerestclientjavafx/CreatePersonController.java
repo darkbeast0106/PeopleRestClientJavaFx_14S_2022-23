61b514b06a48630eec22e7bd6abae7840a02bde8
Budaházi Bence <budahazi.bence@petrik.hu>
2022-11-17 17:03:57 +0100

    Added person form

---
+++ src/main/java/hu/petrik/peoplerestclientjavafx/CreatePersonController.java
@@ -0,0 +1,22 @@
+package hu.petrik.peoplerestclientjavafx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+
+public class CreatePersonController {
+    @FXML
+    private TextField nameField;
+    @FXML
+    private TextField emailField;
+    @FXML
+    private Spinner<Integer> ageField;
+    @FXML
+    private Button submitButton;
+
+    @FXML
+    public void submitClick(ActionEvent actionEvent) {
+    }
+}
